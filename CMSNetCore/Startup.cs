e3dc9fd9f5e6af8e31d9866bda5b24ea3be6a47a
YueXiuFeng <413320490@qq.com>
2019-12-04 12:02:00 +0800

    20191204修改稿

--- CMSNetCore/Startup.cs
+++ CMSNetCore/Startup.cs
@@ -23,6 +23,7 @@ namespace CMSNetCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            //services.Add<AppContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
